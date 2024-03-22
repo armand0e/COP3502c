6b5bcd046f4bf8355951ee9500b08fa4a0a4e38e
armand0e <aran.rafiee@gmail.com>
2024-03-22 17:26:02 -0400

    update

---
+++ Labs/Lab 7/cowsay.py
@@ -0,0 +1,13 @@
+import sys
+
+def list_cows(cows):
+    # Displays the available cows from a Python list of Cow objects.
+    pass
+
+def find_cow(name, cows):
+    # Given a name and a Python list of Cow objects, return the Cow object with the specified name. If no such Cow object can be found, return None.
+    pass
+
+if __name__ == '__main__':
+    if sys.argv[1] == '-l':
+        print("u did -l")
--- debug.py
+++ debug.py
@@ -1,35 +1,55 @@
-def print_board(board):
-    for i in range(len(board)):
-        row = ""
-        for j in range(len(board[i])):
-            row += board[i][j] + " "
-        print(row)
+# Debug!!!!
 
 
-def initialize_board(num_rows, num_cols):
-    board = [['-'] * num_cols] * num_rows
-    return board
+def string_to_rle(rle_string):
+    # 8. Translates a string in human-readable RLE format (with delimiters) into RLE byte data. (Inverse of #7)
+    hex_to_decimal = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+                      '8': 8, '9': 9, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15}
+    
+    sloppy_rle_data = []
+    for i in rle_string:
+        sloppy_rle_data.append(i)
+    
+    sloppy_rle_data.append(":")
+    
+    # search for every delimiter (thats why i added one to the end)
+    last_delimiter_index = 0
+    rle_data = []
+    for i in range(len(sloppy_rle_data)):
+        if sloppy_rle_data[i] != ":":
+            continue
 
+        last_delimiter_index = i
+        hex_digit = sloppy_rle_data[i-1]
+        decimal_digit = sloppy_rle_data[i-2]
+        if i-2 != 0:
+            potential_decimal_digit = sloppy_rle_data[i-3]
+            if potential_decimal_digit != ":":
+                decimal_digit = f'{potential_decimal_digit}{decimal_digit}'
 
-def insert_chip(board, col, chip_type):
-    for i in range(len(board)):
-        for row in range(len(board[i])):
-            if board[row-1][col] == '-':
-                board[row-1][col] = chip_type
-                break
+        hex_digit = hex_to_decimal[hex_digit]
+        
+        rle_data.append(int(decimal_digit))
+        rle_data.append(hex_digit)
 
+    
+    return rle_data
 
-def main():
-    cols = 5
-    rows = 4
-    board = initialize_board(rows, cols)
-    print_board(board)
-    chip = 'x'
-
-    col = int(input('choose column'))
-    insert_chip(board, col, chip)
-    print_board(board)
-
-
-if __name__ == '__main__':
-    main()
+def string_to_data(data_string):
+    # 6. Translates a string in hexadecimal format into byte data (can be raw or RLE). (Inverse of #1)
+    hex_to_decimal = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+                      '8': 8, '9': 9, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15}
+    data = []
+    for i in data_string:
+        data.append(hex_to_decimal[i])
+    return data            
+            
+            
+            
+            
+            
+            
+data = [15, 15, 6, 4]
+# should return ^
+string = "fff444444"
+print(string_to_data(string))
