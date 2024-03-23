0411e6875d38f98c12a8b88211717c35944ae471
armand0e <53029190+armand0e@users.noreply.github.com>
2024-03-23 18:44:54 -0400

    updoot

--- Labs/Lab 6/cow.py
+++ Labs/Lab 6/cow.py
@@ -3,17 +3,18 @@ class Cow:
         # Initializes a cow object with name and image to be None
         # set self.name to be name
         # set self.image to be None
-        pass
+        self.name = name
+        self.image = None
     
     def get_name(self):
         # Returns the name of the cow. Note: the name property should NOT have a setter.
-        pass
+        return self.name
     
     def get_image(self):
         # Returns the image used to display the cow (this should be called after the message has been displayed).
-        pass
+        return self.image
     
     def set_image(self, image):
         # Sets the image used to display the cow
-        pass
+        self.image = image
     
--- Labs/Lab 6/cowsay.py
+++ Labs/Lab 6/cowsay.py
@@ -1,31 +1,65 @@
 import sys
 from heifer_generator import HeiferGenerator
+from cow import Cow
 
 def list_cows(cows):
     # Displays the available cows from a Python list of Cow objects.
-    pass
+    cow_list = ""
+    for cow in cows:
+        
+        cow_list += Cow.get_name(cow) + " "
+    print(f"Cows available: {cow_list}")
 
 def find_cow(name, cows):
     # Given a name and a Python list of Cow objects, return the Cow object with the specified name. If no such Cow object can be found, return None.
+    for i in cows:
+        if Cow.get_name(i) == name:
+            return i
+    return None
+        
     pass
 
 def main():
     cows = HeiferGenerator.get_cows()
-    if sys.argv[1] == '-l':
-        list_cows(cows)
-    elif sys.argv[1] == '-n':
-        try:
-            cow = sys.argv[2]
+    try:
+        if sys.argv[1] == '-l':
+            list_cows(cows)
+        elif sys.argv[1] == '-n':
+            try:
+                name = sys.argv[2]
+                message = ""
+                for i in range(len(sys.argv)):
+                    if i < 3:
+                        continue
+                    message += sys.argv[i] + " "
+                cow = find_cow(name, cows)
+                if cow == None:
+                    print(f"Could not find {name} cow!")
+                else:
+                    print("")
+                    print(message)
+                    print(Cow.get_image(cow))
+                    
+            except IndexError:
+                print("Error: Missing arguments!")
+                print("Try: ") 
+                print(" python3 cowsay.py -n <cow> <message>")
+        elif sys.argv[1] != "-l" and sys.argv[1] != "-n":
+            cow = find_cow("heifer", cows)
             message = ""
             for i in range(len(sys.argv)):
-                if i < 3:
+                if i < 1:
                     continue
-                message += sys.argv[i]
-            cow = find_cow(cow, cows)
-            print(f"cow: {cow}\nmessage: {message}")
-        except IndexError:
-            print("Error: Missing arguements!")
-            print("Try: cowsay.py -n <cow> <message>")
+                message += sys.argv[i] + " "
+            print("")
+            print(message)
+            print(Cow.get_image(cow))
+    except IndexError:
+        print("Error: Missing arguments!")
+        print("Try: ")
+        print(" python3 cowsay.py -l")
+        print(" python3 cowsay.py <message>") 
+        print(" python3 cowsay.py -n <cow> <message>")
 
 if __name__ == '__main__':
     main()
