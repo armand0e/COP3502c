caf50affd4a46ef352f329d3b3e2be335d17e946
armand0e <53029190+armand0e@users.noreply.github.com>
2024-04-18 08:41:53 -0400

    lab10
    made diagonal and star
    todo:
    - finish triangle

--- Labs/Lab 10/abr_lab10.py
+++ Labs/Lab 10/abr_lab10.py
@@ -1,46 +1,62 @@
-def gradient(n, file=None):
-    for i in range(n):
-        for j in range(n):
-            print((i + j) // 2, end=' ', file=file)
-        print(file=file)
-
-
-def square(n, file=None):
-    for i in range(n):
-        for j in range(n):
-            print(min(i, j, n - i - 1, n - j - 1) * 2, end=" ", file=file)
-        print(file=file)
-
-
-def circle(n, file=None):
-    for i in range(n):
-        for j in range(n):
-            distance = ((i - n/2) ** 2 + (j-n/2) ** 2) ** 0.5
-            print(max(int(distance - n ** 0.5), 0),
-                  end=" ", file=file)
-        print(file=file)
-
-def star(size, file=None):
-    starlst = []
+def triangle(size, file=None):
+    center = size // 2 
     for i in range(size):
         for j in range(size):
-            distance = (size - i) // 2
-            
-            starlst.append()
-            print()
-
-def stripes(size, file=None):
+            # top right
+            if j > center:
+                value = size - (size - i + j) // 2 - 1
+            # top left
+            if j <= center:
+                value = (i + j) // 2 
+            value = max(int((value*2 - size ** 0.5)),0)
+            if value > size:
+                value = size    
+            print(value, end=" ", file=file)
+        print(file=file)  
+        
+def diagonal(size, file=None):
     for i in range(size):
         for j in range(size):
-            if j == 0 and i != 0:
-                potential_newline = "\n"
-            else:
-                potential_newline = ""
-            if j % 2 == 0:
-                print(f"{potential_newline}0", end=" ", file=file)
-            else:
-                print(f"{potential_newline}{size}", end=" ", file=file)
-    print(file=file)
+            distance = int(((i - i)**2 + (j - i)**2)**0.5)
+            value = max(int((distance*1.5 - size ** 0.5)),0)
+            if value > size:
+                value = size
+            value = size - value
+            print(value, end=" ", file=file)
+        print(file=file)
+        
+def star(size, file=None):                
+    for i in range(size):
+        for j in range(size):
+            center = size // 2            
+
+            if i < center:
+                # top right
+                if j > center:
+                    value = size - (size - i + j) // 2 - 1
+                # top left
+                if j < center:
+                    value = (i + j) // 2
+            if i > center:
+                # bottom right
+                if j > center:
+                    value = size - (i + j) // 2 - 1
+                #bottom left
+                if j < center:
+                    value = (size - i + j) // 2
+            if i == center or j == center:
+                value = (i+j)//2
+                if j > center:
+                    value = size - (i+j)//2 - 1
+                if i > center:
+                    value = size - (i+j)//2 - 1
+            if value == center:
+                value -= 1
+            value = max(int((value - size ** 0.5)),0)
+            if value > size:
+                value = size    
+            print(value, end=" ", file=file)  
+        print(file=file)
     
 def make_image(pattern, size, filename):
     if size < 100:
@@ -50,7 +66,8 @@ def make_image(pattern, size, filename):
     file.write(str(pattern(size, file)))
 
 def main():
-    make_image(circle, 100, "circle.pgm")
+    triangle(25)
+    make_image(triangle, 100, "triangle.pgm")
 
 if __name__ == "__main__":
     main()
